7e41993b91c1cbf4a9aadee92415db793455488e
Gaurav7bajaj <gauravbajaj0425@gmail.com>
2026-06-21 22:53:46 +0530

    Fix: Resolve homepage 404 error on Vercel by updating index.js exports

--- Frontend/app/components/home/index.js
+++ Frontend/app/components/home/index.js
@@ -1,7 +1,8 @@
-export { default as RomanceEscapesSection } from "./RomanceEscapesSection";
-export { default as HeroSection } from "./HeroSection";
-export { default as IndiaTripsSection } from "./IndiaTripsSection";
-export { default as InternationalTripsSection } from "./InternationalTripsSection";
-export { default as ReviewsSection } from "./ReviewsSection";
-export { default as UpcomingTripsSection } from "./UpcomingTripsSection";
-export { default as WhyDivasSection } from "./WhyDivasSection";
+export { default as HeroSection } from './HeroSection';
+export { default as ReviewsSection } from './ReviewsSection';
+export { default as UpcomingTripsSection } from './UpcomingTripsSection';
+export { default as IndiaTripsSection } from './IndiaTripsSection';
+export { default as InternationalTripsSection } from './InternationalTripsSection';
+export { default as RomanceEscapesSection } from './RomanceEscapesSection';
+export { default as WhyDivasSection } from './WhyDivasSection';
+
